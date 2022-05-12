c4a56f0a97cb2984b7b4c6129d70cff563c475da
johoon-Choi <johoon4687@gmail.com>
2022-05-13 00:55:28 +0900

    220513

--- reference1.py
+++ reference1.py
@@ -1,7 +1,6 @@
 # code by Tae Hwan Jung(Jeff Jung) @graykode
 # Reference : https://github.com/prakashpandey9/Text-Classification-Pytorch/blob/master/models/LSTM_Attn.py
 import numpy as np
-import os
 import torch
 import torch.nn as nn
 import torch.optim as optim
@@ -53,23 +52,24 @@ if __name__ == '__main__':
 
         if str == "":
             break
-
+        str = str.strip()
         print(" str :", str)
 
         if str[-1] == ";" or str[-2] == ";":
             colon_idx = str.find(";")
             str = str[0:colon_idx]
-            print(" str in ; phase {", str, "}")
-            input_str = input_str + str
+
+            input_str = input_str + " " + str + '\n'
+            print("---input_str--->", input_str)
+            mq.write(input_str)
             input_str = ""
         elif str[-1] == '\n':
             str = str[:len(str) - 1]
-            print(" str in \n phase {", str, "}")
-            input_str = input_str + str
+
+            input_str = input_str + " " + str
         else:
-            input_str = input_str + str
+            input_str = input_str + " " + str
         print(" ---input_str--->", input_str)
-        mq.write(input_str)
     mq.close()
 
     # 3 words sentences (=sequence_length is 3)
@@ -128,5 +128,3 @@ if __name__ == '__main__':
     ax.set_xticklabels(['']+['first_word', 'second_word', 'third_word'], fontdict={'fontsize': 14}, rotation=90)
     ax.set_yticklabels(['']+['batch_1', 'batch_2', 'batch_3', 'batch_4', 'batch_5', 'batch_6'], fontdict={'fontsize': 14})
     # plt.show()
-
-    os.remove(mq)
