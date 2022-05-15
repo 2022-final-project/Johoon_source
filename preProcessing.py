5c860e28b47937fdacba624996266fd75361365a
johoon-Choi <johoon4687@gmail.com>
2022-05-15 23:31:31 +0900

    220515

--- preProcessing.py
+++ preProcessing.py
@@ -31,6 +31,7 @@ class preProcessing():
         cur_word = cur_word.split("(")
         cur_word = cur_word[0]
         if (cur_word.find("'") != -1):
+            if (cur)
             print(" word : ", cur_word, " 에서 delete state == ", self.delete_state)
             if self.delete_state == False:
                 self.delete_state = True
