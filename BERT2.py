7944257c476485f62001410463fbba7ea576e03c
johoon-Choi <johoon4687@gmail.com>
2022-05-19 00:55:23 +0900

    22.05.19

--- BERT2.py
+++ BERT2.py
@@ -2,12 +2,16 @@ from transformers import BertTokenizer
 from transformers import BertModel
 from transformers import BertConfig
 from transformers import BertForMultipleChoice
+from transformers import BertForPreTraining
+import position
 import torch
 
 configuration = BertConfig(500)
 tokenizer = BertTokenizer.from_pretrained("./vocab.txt")
 model1 = BertModel(configuration)
 model2 =  BertForMultipleChoice(configuration)
+model3 = BertForPreTraining(configuration)
+model4 = position.PositionalEmbedding()
 
 configuration1 = model1.config
 configuration2 = model2.config
@@ -21,17 +25,29 @@ choice4 = "16.0"
 choice5 = "32.0"
 labels = torch.tensor(0).unsqueeze(0)  
 
-encoding = tokenizer([prompt, prompt, prompt, prompt, prompt, prompt], [choice0, choice1, choice2, choice3, choice4, choice5], return_tensors="pt", padding=True, truncation=True)
-outputs2 = model2(**{k: v.unsqueeze(0) for k, v in encoding.items()}, labels=labels)
+# encoding = tokenizer([prompt, prompt, prompt, prompt, prompt, prompt], [choice0, choice1, choice2, choice3, choice4, choice5], return_tensors="pt", padding=True, truncation=True)
+# outputs2 = model2(**{k: v.unsqueeze(0) for k, v in encoding.items()}, labels=labels)
 
 inputs1 = tokenizer("select c1 from t1", return_tensors="pt")
 outputs1 = model1(**inputs1)
+outputs3 = model3(**inputs1)
 
-print("[input]\n", inputs1)
-print("[outputs]\n", outputs1) 
+print("[inputs 1]\n", inputs1)
+print("[outputs 1]\n", outputs1)
+print("[outputs 3]\n", outputs3) 
 
-loss = outputs2.loss
-logits = outputs2.logits
+# loss = outputs2.loss
+# logits = outputs2.logits
 
-print("loss : ", loss)
-print("logits : ", logits)
+prediction_logits = outputs3.prediction_logits
+seq_relationship_logits = outputs3.seq_relationship_logits
+
+# print("loss : ", loss)
+# print("logits : ", logits)
+
+'''
+
+output1 : normal 하게 하는거
+output2 : multipleChoice 로 하는거
+
+'''
